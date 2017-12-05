8c01af67e11d7de9602c12e043751523a57d73e7
joao <joaoguilhermeos1@gmail.com>
2017-12-04 22:56:56 -0200

    machucando player

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -21,6 +21,7 @@ public class GM : MonoBehaviour {
 	PlayerCtrl player;
 
 	public float TimeToRespawn = 2f;
+	public float timeToKill = 1.5f;
 
 	public UI ui;
 
@@ -129,5 +130,32 @@ public class GM : MonoBehaviour {
 		ui.levelComplete.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 		ui.levelComplete.LevelCompletePanel.SetActive(true);
 
+	}
+
+	public void HurtPlayer(){
+		if (player != null){
+			DisableAndPushPlayer();
+			Destroy(player.gameObject, timeToKill);
+			DecrementLives();
+			if (data.lifeCount > 0){
+				Invoke("RespawnPlayer", timeToKill + TimeToRespawn);
+			}
+			else {
+				GameOver();
+			}
+		}
+	}
+
+	void DisableAndPushPlayer(){
+		player.transform.GetComponent<PlayerCtrl>().enabled = false;
+		foreach (Collider2D c2d in player.transform.GetComponents<Collider2D>()){
+			c2d.enabled = false;
+		}
+		foreach (Transform child in player.transform){
+			child.gameObject.SetActive(false);
+		}
+		Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.zero;
+		rb.AddForce(new Vector2(-150.0f, 400f));
 	}
 }
